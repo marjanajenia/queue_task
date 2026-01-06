84a5b37c8897ff0c9d901b48511b2775b01f535d
marjanajenia <marjanajenia98@gmail.com>
2026-01-07 00:16:52 +0600

    relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,5 +47,9 @@ class User extends Authenticatable
             'last_login_at' => 'datetime',
         ];
     }
+    public function reminders(){
+        return $this->hasMany(UserReminder::class);
+    }
+    
 
 }
