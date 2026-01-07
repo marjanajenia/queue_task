8b95b7f5dc064d81a52ceef7c492adb5cccc1633
marjanajenia <marjanajenia98@gmail.com>
2026-01-07 21:22:09 +0600

    schedule

---
+++ app/Console/Commands/SendInactiveUserReminders.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class SendInactiveUserReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:send-inactive-user-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reminder to inactive user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $sevenDaysAgo = now() -> subDays(7);
+
+        $users = User::whereNotNull('last_login_at')
+        ->where('last_login_at', '<=', $sevenDaysAgo)
+        ->whereDoesntHave('reminders', function ($q){
+            $q->where('sent_on', now()->toDateString());
+        })
+        ->get();
+        foreach ($users as $user) {
+            SendInactiveUserReminder::dispatch($user);
+        }
+
+        $this->info('Inactive user reminders dispatched.');
+    }
+}
